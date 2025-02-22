ad87d0beae10f09ee625d196dd8d382c71a6a358
annsinkevich47 <anneta.sinkevich@mail.ru>
2025-02-22 23:58:20 +0300

    feat: add subs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InnerComponent } from './inner/inner.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InnerComponent, RxjsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
