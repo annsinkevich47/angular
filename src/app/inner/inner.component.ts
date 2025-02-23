2b67af87740343401b626d36e2abb5ab71311bdc
annsinkevich47 <anneta.sinkevich@mail.ru>
2025-02-23 22:11:12 +0300

    feat: add signals

--- src/app/inner/inner.component.ts
+++ src/app/inner/inner.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, computed, effect, OnInit, signal } from '@angular/core';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-inner',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './inner.component.html',
   styleUrl: './inner.component.scss'
 })
 export class InnerComponent {
 
+  count = signal(0);
+  doubleCount = computed(() => this.count() * 2);
+  items = signal<{ title: string }[]>([]);
+
+  constructor(private http: HttpClient) {
+    effect(() => {
+      console.log(this.count())
+    })
+  }
+
+  increment() {
+    this.count.set(this.count() + 3);
+  }
+  loadItems() {
+    this.http.get<{ title: string }[]>('https://jsonplaceholder.typicode.com/posts').pipe(tap(value => console.log(value))).subscribe(value => this.items.set(value))
+  }
 }
--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -1,22 +1,30 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { map, Observable, of, Subscription, takeUntil } from 'rxjs';
+import { combineLatest, distinctUntilChanged, interval, map, mergeMap, Observable, of, skip, Subscription, take, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './rxjs.component.html',
   styleUrl: './rxjs.component.scss'
 })
 export class RxjsComponent {
+
+  constructor(private http: HttpClient) { }
   number$ = of(1, 1, 2, 3).pipe(
-    map(value => value >= 2 ? value : value * 3)
+    map(value => value >= 2 ? value : value * 3),
+    tap(value => console.log(value)),
+    skip(3)
   );
   number1$ = of(4, '5', [4, 5]).pipe(
     map(value => isNaN(Number(value)) ? value : Number(value) - 7)
   );
   number2$ = of(7, 1, 4, 9);
 
+  intervalOne$ = interval(100).pipe(take(9));
+
+  mergeFlow$ = interval(1000).pipe(mergeMap(() => this.http.get('https://jsonplaceholder.typicode.com/posts/1')))
   private subscristions = new Subscription();
 
   numbersObs$ = new Observable((sub) => {
@@ -28,10 +36,16 @@ export class RxjsComponent {
 
   showNumber(num: Observable<unknown>) {
     const sub1 = num.pipe(map(value => isNaN(Number(value)) ? value : value + `fsdf`)).subscribe(value => console.log(value));
-    const sub2 = this.numbersObs$.subscribe((value) => console.log(value));
+    // const sub2 = this.numbersObs$.subscribe((value) => console.log(value));
+    // const sub3 = this.intervalOne$.subscribe((value) => console.log(value));
+    // // const sub4 = this.mergeFlow$.subscribe((value) => console.log(value));
+    // const sub5 = combineLatest([this.number2$, this.intervalOne$]).subscribe((value) => console.log(value))
 
     this.subscristions.add(sub1);
-    this.subscristions.add(sub2);
+    // this.subscristions.add(sub2);
+    // this.subscristions.add(sub3);
+    // // this.subscristions.add(sub4);
+    // this.subscristions.add(sub5);
     console.log(this.subscristions);
   }
 
